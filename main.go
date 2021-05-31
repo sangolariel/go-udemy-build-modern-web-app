5703043928efc1ea757d3c0a30ad5568c56bf8d2
sangnh <sangnh@futurify.vn>
2021-05-31 17:31:03 +0700

    func and handle

--- main.go
+++ main.go
@@ -5,13 +5,33 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, "Hello world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-	})
+const port = ":8080"
 
-	_ = http.ListenAndServe(":8080", nil)
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Home Page!")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(1, 2)
+	_, err := fmt.Fprintf(w, "About Page")
+	fmt.Fprintf(w, fmt.Sprintf("Sum: %d", sum))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Aplication open on port %s", port))
+
+	_ = http.ListenAndServe(port, nil)
 }
