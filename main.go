a6192deac42f8e9577aa6e508466c6cc270371d8
sangnh <sangnh@futurify.vn>
2021-05-31 23:03:45 +0700

    Error checking

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -27,9 +28,28 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Devide(w http.ResponseWriter, r *http.Request) {
+	f, err := devideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Can not divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f devided %f is % f", 100.0, 10.0, f))
+}
+
+func devideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Can not devide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/devide", Devide)
 
 	fmt.Println(fmt.Sprintf("Aplication open on port %s", port))
 
