4e678c2bec9e2554ff03fdfa064cd56389e1109a
sangnh <sangnh@futurify.vn>
2021-06-08 22:15:01 +0700

    9. Working with Layouts, and building a template cache (part one)

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,12 +4,53 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
+var function = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	_, err := RenderTemplateTest(w)
+	if err != nil {
+		fmt.Println("Error getting template catche", err)
+	}
+
 	parsedTemp, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemp.Execute(w, nil)
+	err = parsedTemp.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template", err)
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+	myCatche := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return myCatche, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page is currently", page)
+		ts, err := template.New(name).Funcs(function).ParseFiles(page)
+		if err != nil {
+			return myCatche, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCatche, err
+		}
+
+		if len(matches) > 0 {
+			ts, err := ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCatche, err
+			}
+			myCatche[name] = ts
+		}
+	}
+	return myCatche, err
 }
