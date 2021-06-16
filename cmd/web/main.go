6f019db810c102241396b128e5453c4667f6b37b
sangnh <sangnh@futurify.vn>
2021-06-16 23:05:48 +0700

    1. Installing and setting up a sessions package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/sangolariel/go-udemy-build-modern-web-app/pkg/config"
 	"github.com/sangolariel/go-udemy-build-modern-web-app/pkg/handlers"
 	"github.com/sangolariel/go-udemy-build-modern-web-app/pkg/render"
@@ -12,8 +14,21 @@ import (
 
 const port = ":8080"
 
+var app config.AppConfig
+
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+	//Env
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
 	tc, err := render.CreateTemplateCatche()
 	if err != nil {
 		log.Fatal("Can't create Template catche")
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"html/template"
 	"log"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 // AppConfig holds the application config
@@ -10,4 +12,6 @@ type AppConfig struct {
 	UseCatche      bool
 	TemplateCatche map[string]*template.Template
 	Info           *log.Logger
+	InProduction   bool
+	Session        *scs.SessionManager
 }
